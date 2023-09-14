38a0a063fa7e875e016ae8ddb48bfde445c00f1a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-14 09:24:45 +0100

    added mongo setup

--- server/index.js
+++ server/index.js
@@ -34,3 +34,12 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+
+})
+
