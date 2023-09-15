c69bd7c08026245a2440086654d9a62a827f526a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-15 12:25:20 +0100

    added some changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,39 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.js');
+
+// REGISTER USER
+exports.register = async (req, res) =>{
+    try{
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturepath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturepath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err){
+
+    }
+}
