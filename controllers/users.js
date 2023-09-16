2bf1006e850b9b37f4bc266fa5c6ae483db04dbf
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-16 12:34:52 +0100

    added a comment

--- controllers/users.js
+++ controllers/users.js
@@ -43,9 +43,28 @@ const addRemoveFriend = async (req, res) => {
             friend.friends = friend.friends.filter((id) => id !== id);
         } else {
             user.friends.push(friendId);
+            friend.friends.push(id);
         }
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return {_id, firstName, lastName, occupation, location, picturePath};
+            }
+        );
+        res.status(200).json(formattedFriends);
 
     } catch (err) {
         res.status(404).json({message: err.message});
     }
+}
+
+module.exports = {
+    getUser,
+    getUserFriends,
+    addRemoveFriend
 }
