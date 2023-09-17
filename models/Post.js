e0a56a0c543882014ebe23030da34e033fa2d6fe
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-17 14:21:37 +0100

    added a schema

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,27 @@ const postSchema = mongoose.Schema(
         userId: {
             type: String,
             required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        location: String,
+        description: String,
+        picturePath: String,
+        userPicturePath: String,
+        likes: {
+            type: Map,
+            of: Boolean,
+        },
+        comments: {
+            types: Array,
+            default: []
         }
-    }
+    },
+    { timestamps: true }
 )
