8cd3379ab7f88a41282344a0abfccb89d1fd00cb
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-09-17 14:59:41 +0100

    added like post

--- controllers/posts.js
+++ controllers/posts.js
@@ -40,8 +40,28 @@ const getFeedPosts = async (req, res) => {
 
 const getUserPosts = async (req, res) => {
     try {
-
+        const { userId } = req.params;
+        const post = await Post.find({ userId });
+        res.status(200).json(post);
     } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
 
+// UPDATE
+const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if(isLiked) {
+            post.likes.delete(userId);
+        }
+
+        res.status(200).json();
+    } catch (err) {
+        res.status(404).json({ message: err.message });
     }
 };
